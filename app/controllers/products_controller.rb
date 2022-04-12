9c736936c128848727e9587aac1292df3297ef84
m226360 <m226360@usna.edu>
2022-04-12 11:43:27 -0400

    website is up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @cart = session[:cart]
   end
 
 def show
@@ -8,17 +9,30 @@ def show
 end
 
 def buy
+
+
   if session[:cart].nil?
     session[:cart] = []
   end
-  product = Product.find(params[:id])
-  session[:cart].append(product)
+  @product = Product.find(params[:id])
+  session[:cart].append(@product)
   redirect_to :root
 end
-def checkout
+def cart
   @cart = session[:cart]
+  #session[:cart] = []
+end
+
+def order
+  @cart = session[:cart]
+  @cart.each do |item|
+    p=Product.find(item["id"])
+    p.quantity = p.quantity-1
+    p.save
+  end
   session[:cart] = []
 end
+
 
 private
 def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root "products#index"
     get "/products", to: "products#index"
     get '/products/:id/buy', to: 'products#buy', as: 'buy'
-    get '/checkout', to: 'products#checkout', as: 'checkout'
+    get '/cart', to: 'products#cart', as: 'cart'
     get "products.json", to: "products#list"
+    get '/order', to: 'products#order', as: 'order'
   
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
